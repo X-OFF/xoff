f677e448149b25fe7605cf6484ab65ea9fa8a9ca
Jackson Hayes <jacksonjhayes@outlook.com>
2017-03-27 13:26:56 -0500

    Add a api to test against

---
+++ OfflineFirstReference.Web.DAL/Models/Widget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OfflineFirstReference.Web.DAL.Models
+{
+    public class Widget
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LastTimeSynced { get; set; }
+    }
+}
---
+++ OfflineFirstReference.Web.DAL/WidgetContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfflineFirstReference.Web.DAL.Models;
+
+namespace OfflineFirstReference.Web.DAL
+{
+    public class WidgetContext : DbContext
+    {
+        public WidgetContext(DbContextOptions<WidgetContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Widget> Widgets { get; set; }
+          
+    }
+}
